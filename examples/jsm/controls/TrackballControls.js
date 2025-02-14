82105b1c7387abdf26db5fa2acbb7dfd9fa1fcdd
Selling Solona <194745542+sellingsolona@users.noreply.github.com>
2025-02-14 00:54:34 -0500

    Update TrackballControls.js

--- examples/jsm/controls/TrackballControls.js
+++ examples/jsm/controls/TrackballControls.js
@@ -1,4 +1,4 @@
-import * as THREE from '../../build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
 
 const { Controls, MathUtils, MOUSE, Quaternion, Vector2, Vector3} = THREE;
 
