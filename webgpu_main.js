4ea30bd3ef18b43ff992d30ae7353ffa8faa1bf6
Selling Solona <194745542+sellingsolona@users.noreply.github.com>
2025-02-16 01:02:25 -0500

    Update webgpu_main.js

--- webgpu_main.js
+++ webgpu_main.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import * as TSL from 'three/nodes';
-import WebGPURenderer from 'three/examples/jsm/renderers/webgpu/WebGPURenderer.js';
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGPURenderer();
+
 import { OrbitControls } from 'three/examples/controls/OrbitControls.js';
 import { GUI } from 'three/examples/libs/lil-gui.module.min.js';
 import WebGPU from 'three/examples/capabilities/WebGPU.js';
